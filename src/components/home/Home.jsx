83724476d81a79dec851b011d9bc7c118fec106c
leeport511 <leonardoandres.portillo@gmail.com>
2023-09-06 18:17:50 -0300

    add bg to home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className="flex flex-col  justify-center items-center h-[80vh]">
+    <section className="flex flex-col  justify-center items-center h-[80vh] backgroundSVG-home">
         <img src="../../assets/images/computadora-de-escritorio.png" alt="Logo"/>
         <h2 className=" font-montserrat font-bold tracking-[3px] mt-2 text-4xl text-orange">FAKE E STORE</h2>
         <p  className="mt-6 font-hindMadurai tracking-[2px] text-xl text-greenBlue font-medium capitalize">A place to find the best resources for upgrade or have a new computer.</p>
