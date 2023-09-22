e6ff88117668ae9a6f505a99fbd23156f17a56f2
leeport511 <leonardoandres.portillo@gmail.com>
2023-09-22 18:57:57 -0300

    add tap style

--- src/components/SideNavBar/MenuFloatButton.jsx
+++ src/components/SideNavBar/MenuFloatButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const MenuFloatButton = ({toogleMenuDisplay, handleMenuDisplay}) => {
   return (  
-    <button className={`absolute ${toogleMenuDisplay ? 'left-48 top-28 transition-all ease-out duration-200' : 'left-3 top-28 transition-all ease-out duration-200' }  bg-[#f8f8f8] rounded-full w-[60px] border-ligthOrange border-4 shadow-menuButton z-50 xl:hidden`} onClick={handleMenuDisplay}>
+    <button className={`absolute ${toogleMenuDisplay ? 'left-48 top-28 transition-all ease-out duration-200' : 'left-3 top-28 transition-all ease-out duration-200' } unset bg-[#f8f8f8] rounded-full w-[60px] border-ligthOrange border-4 shadow-menuButton z-50 xl:hidden`} onClick={handleMenuDisplay}>
         <img src="../../assets/images/bar-de-hamburguesas.png" alt="Logo Fake Store" className=""/>
     </button>
   )
